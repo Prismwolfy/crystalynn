20ec809c49394de0ee2e74bf791c542a1e9825f6
Crystal Meyer <prismwolf@gmail.com>
2025-08-14 15:36:20 -0600

    trying to fix css issues in prod

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const isProd = process.env.NODE_ENV === 'production';
 
+const nextConfig = {
+  output: 'export', // Required for static export to GitHub Pages
+  basePath: isProd ? '/crystalynn' : '',
+  assetPrefix: isProd ? '/crystalynn/' : '',
+  images: {
+    unoptimized: true, // Recommended for GitHub Pages as it doesn't support Next.js image optimization
+  },
+};
 
 export default nextConfig;
